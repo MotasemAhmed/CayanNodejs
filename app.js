00e73740fd5f3f675f3fa8b30ec7bd505043f550
Motasem94 <amotasem.ahmed@gmail.com>
2022-04-10 11:15:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+const port = 3000;
+
+// const db = mysql.createConnection({
+//   host: "153.92.6.148",
+//   port: "3306",
+//   user: "u697102081_testHostdb",
+//   password: ":P2;n&dT2j",
+//   database: "u697102081_TestDB",
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("DB Connected");
+// });
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  acquireTimeout: 10000,
+  host: "153.92.6.148",
+  port: "3306",
+  user: "u697102081_testHostdb",
+  password: ":P2;n&dT2j",
+  database: "u697102081_TestDB",
+});
+
+app.get("/test", function (req, res, next) {
+  var sql = "SELECT * FROM test";
+  pool.query(sql, function (err, data, fields) {
+    if (err) throw err;
+    const result = Object.values(JSON.parse(JSON.stringify(data)));
+    console.log(result);
+    res.send(result);
+  });
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
