8c42aedcc6e29fdb57238187abc135f7cecf7c8e
Motasem94 <amotasem.ahmed@gmail.com>
2022-04-13 00:53:33 +0200

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = 5000;
 
 // const db = mysql.createConnection({
 //   host: "153.92.6.148",
